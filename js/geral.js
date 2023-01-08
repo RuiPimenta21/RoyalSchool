3b5f251c7ad0e844148e0486000e9f007e320f4e
Rui Pimenta <a21806224@mso365.ulp.pt>
2023-01-08 03:24:39 +0000

    commit

--- js/geral.js
+++ js/geral.js
@@ -1,4 +1,10 @@
 const toggleMenuOpen = () => document.body.classList.toggle("open");
 
-
-
+function mostrarPassword() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
