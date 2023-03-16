4448efe91d33d0fd550f857ec37206c495c73f85
Rui Pimenta <a21806224@mso365.ulp.pt>
2023-03-16 01:35:03 +0000

    commit

--- js/perfil/mostrarPerfil.js
+++ js/perfil/mostrarPerfil.js
@@ -1,10 +1,12 @@
 let utilizadorId = "";
+let utilizadorPassword = "";
 
 
 carregaDados_mostarPerfil();
 
 function carregaDados_mostarPerfil(){
     utilizadorId = document.forms["formPerfil"]["txtId"].value;
+    utilizadorPassword = document.forms["formPerfil"]["txtPassword"].value;
 
     xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
@@ -22,7 +24,11 @@ function carregaDados_mostarPerfil(){
 
     //operacao está CONCLUIDA e resposta está OK
     if (this.readyState == 4 && this.status == 200) {
-        console.lo(mensagem[0]);
+        console.log(mensagem[0]);
+        console.log(mensagem[1]);
+        console.log(mensagem[2]);
+
+        // aqui manda valores para textbox
     }
     else{    
         document.getElementById("txtErro").style.display = "block";
@@ -30,7 +36,7 @@ function carregaDados_mostarPerfil(){
         document.getElementById("txtErro").innerHTML = "ReadyState do pedido: " + this.readyState + ";  Status da resposta: " + this.status + "; Erro: " + this.responseText + ";";  
     };
     //Encoding
-    xhttp.open("GET", "php/perfil/mostrarDados.php?id="+encodeURIComponent(id)+"&descritivo="+encodeURIComponent(descritivo)+"&valorInscricao="+encodeURIComponent(valorInscricao)+"&valorLivro="+encodeURIComponent(valorLivro)+"&valorMensalidade="+encodeURIComponent(valorMensalidade), true);
+    xhttp.open("GET", "php/perfil/mostrarDados.php?id="+encodeURIComponent(utilizadorId)+"&password="+encodeURIComponent(utilizadorPassword), true);
     xhttp.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
     xhttp.send();
     }
--- php/perfil/mostraDados.php
+++ php/perfil/mostraDados.php
@@ -6,54 +6,30 @@
    define('DB_DRIVER'      , "sqlsrv");
 
    //decoding 
-   $txtNumeroRegistos = rawurldecode($_REQUEST["numeroRegistos"]);
-   $txtOrdenacaoRegistos = rawurldecode($_REQUEST["ordenacaoRegistos"]);
-   $txtPesquisaDescritivo = rawurldecode($_REQUEST["pesquisaDescritivo"]);
+   $txtId = rawurldecode($_REQUEST["utilizadorId"]);
+   $txtPassword = rawurldecode($_REQUEST["utilizadorPassword"]);
 
    require_once "../Conexao.php";
    try{
       $Conexao = Conexao::getConnection();
 
       $stringQuery="";
-      $stringQuery = "exec rpt_mostraTipoInscricao @numeroRegistos = '".$txtNumeroRegistos."',  @pesquisaDescritivo = '".$txtPesquisaDescritivo."', @ordenacaoRegistos = '".$txtOrdenacaoRegistos."'";
-     
+      $stringQuery = "exec rpt_mostraPerfil @id = '".$txtId."',  @password = '".$txtPassword."'";
+      
       $query = $Conexao->query($stringQuery);
       $dados = $query->fetchAll();
+
+      $linha=0;
+      foreach($dados as $dado) {
+         $dado[$linha];
+         $linha++;
+      }
+      $myArr = $dado;
+      $myJSON = json_encode($myArr);
+      echo $myJSON;
+
    }catch(Exception $e){
       echo $e->getMessage();
       exit;
    }
 ?>
-
-<!DOCTYPE html>
-<html>
-<body>
-   <table id="tblTipoInscricao" class="table table-striped table table-bordered table-striped mb-0 tableFixHead">
-      <thead class="tabelaCabecalho ">
-         <tr>
-            <th style="display:none;" scope="col">Id</th>
-            <th scope="col">Nº</th>
-            <th scope="col">Descrição</th>
-            <th scope="col">Valor Inscrição Sem Iva</th>
-            <th scope="col">Valor Livro Sem Iva</th>
-            <th scope="col">Valor Mensalidade Sem Iva</th>
-         </tr>
-      </thead>
-      <tbody>
-         <?php $linha=0;
-         foreach($dados as $dado) {
-            $linha++;
-         ?>
-         <tr onclick="selecionaLinha_TipoInscricao(<?php echo(json_encode($linha));?>)">
-            <td style="display:none;" class="tabelaDadosTexto" ><?php echo $dado['tipoInscricao_id'];?></td>
-            <td scope="row"><?php echo $linha;?></td>
-            <td class="tabelaDadosTexto" ><?php echo $dado['tipoInscricao_descricao'];?></td>
-            <td class="tabelaDadosNumeros"><?php echo $dado['tipoInscricao_valorIscricaoSemIva'];?></td>
-            <td class="tabelaDadosNumeros"><?php echo $dado['tipoInscricao_valorLivroSemIva'];?></td>
-            <td class="tabelaDadosNumeros"><?php echo $dado['tipoInscricao_valorMensalidadeSemIva'];?></td>
-         </tr>
-         <?php
-         }
-         ?>
-      </tbody>
-   </table>
